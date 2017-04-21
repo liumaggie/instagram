f3cb8f6e5ce416ef54fbc2fe0810a22959a14ffd
Maggie Liu <mliu0805@gmail.com>
2017-04-20 22:54:16 -0400

    Edit config file to test if heroku successfully seeds the data

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module Instagram
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_region => ENV['AWS_REGION'],
+      :bucket => ENV['AWS_BUCKET'],
       :s3_credentials => {
-        :bucket => ENV['AWS_BUCKET'],
         :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
       }
