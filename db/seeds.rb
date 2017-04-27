b520378f61ad0c0a01db40884d0c7ded23a8a505
Maggie Liu <mliu0805@gmail.com>
2017-04-27 01:58:36 -0400

    Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,6 +44,45 @@ user9 = User.create(username: Faker::Pokemon.unique.name,
 user10 = User.create(username: Faker::Pokemon.unique.name,
                     password: "password", bio: Faker::StarWars.unique.quote,
                     prof_image: File.open("app/assets/images/puppy.jpg"))
+user11 = User.create(username: Faker::Pokemon.unique.name,
+                    password: "password", bio: Faker::StarWars.unique.quote,
+                    prof_image: File.open("app/assets/images/puppy4.jpg"))
+user22 = User.create(username: Faker::Pokemon.unique.name,
+        password: "password",
+        bio: "Hi :) I love traveling and bringing my puppy with me everywhere I go!",
+        prof_image: File.open("app/assets/images/puppy15.jpg"))
+user12 = User.create(username: Faker::Pokemon.name,
+        password: "password",
+        prof_image: File.open("app/assets/images/puppy34.jpg",
+        bio: "Hello world!"))
+user13 = User.create(username: Faker::Pokemon.unique.name,
+        password: "password", bio: Faker::StarWars.unique.quote)
+user14= User.create(username: Faker::Pokemon.unique.name,
+        password: "password",
+        bio: Faker::StarWars.unique.quote,
+        prof_image: File.open("app/assets/images/puppy51.jpg"))
+user15 = User.create(username: Faker::Pokemon.unique.name,
+        password: "password", bio: Faker::StarWars.unique.quote)
+user16 = User.create(username: Faker::Pokemon.unique.name,
+        password: "password",
+        bio: Faker::StarWars.unique.quote,
+        prof_image: File.open("app/assets/images/puppy28.jpg"))
+user17 = User.create(username: Faker::Pokemon.unique.name,
+        password: "password", bio: Faker::StarWars.unique.quote,
+        prof_image: File.open("app/assets/images/puppy32.jpg"))
+user18 = User.create(username: Faker::Pokemon.unique.name,
+        password: "password", bio: Faker::StarWars.unique.quote,
+        prof_image: File.open("app/assets/images/puppy46.jpg"))
+user19 = User.create(username: Faker::Pokemon.unique.name,
+        password: "password", bio: Faker::StarWars.unique.quote,
+        prof_image: File.open("app/assets/images/puppy50.jpg"))
+user20 = User.create(username: Faker::Pokemon.unique.name,
+        password: "password", bio: Faker::StarWars.unique.quote,
+        prof_image: File.open("app/assets/images/puppy45.jpg"))
+user21 = User.create(username: Faker::Pokemon.unique.name,
+        password: "password", bio: Faker::StarWars.unique.quote,
+        prof_image: File.open("app/assets/images/puppy39.jpg"))
+
 
 image1 = Image.create(
                 image: File.open("app/assets/images/puppy.jpg"),
@@ -53,7 +92,7 @@ image2 = Image.create(image: File.open("app/assets/images/puppy2.jpg"), user_id:
 image3 = Image.create(image: File.open("app/assets/images/puppy3.jpg"), user_id: user1.id)
 image4 = Image.create(image: File.open("app/assets/images/puppy4.jpg"), user_id: user1.id)
 image5 = Image.create(image: File.open("app/assets/images/pup.jpg"), user_id: user1.id)
-image6 = Image.create(image: File.open("app/assets/images/puppy5.jpg"), user_id: user1.id)
+image6 = Image.create(image: File.open("app/assets/images/puppy5.jpg"), user_id: user2.id)
 image7 = Image.create(image: File.open("app/assets/images/puppy6.jpg"), user_id: user2.id)
 image8 = Image.create(image: File.open("app/assets/images/puppy7.jpg"), user_id: user3.id)
 image9 = Image.create(image: File.open("app/assets/images/puppy8.jpg"), user_id: user2.id)
@@ -68,13 +107,41 @@ image17 = Image.create(image: File.open("app/assets/images/puppy16.jpg"), user_i
 image18 = Image.create(image: File.open("app/assets/images/puppy17.jpg"), user_id: user6.id)
 image19 = Image.create(image: File.open("app/assets/images/puppy18.jpg"), user_id: user6.id)
 image20 = Image.create(image: File.open("app/assets/images/puppy19.jpg"), user_id: user7.id)
-image21 = Image.create(image: File.open("app/assets/images/puppy20.jpg"), user_id: user8.id)
+image21 = Image.create(image: File.open("app/assets/images/puppy20.jpg"), user_id: user1.id)
 image22 = Image.create(image: File.open("app/assets/images/puppy21.jpg"), user_id: user9.id)
 image23 = Image.create(image: File.open("app/assets/images/puppy22.jpg"), user_id: user10.id)
 image24 = Image.create(image: File.open("app/assets/images/puppy23.jpg"), user_id: user10.id)
 image25 = Image.create(image: File.open("app/assets/images/puppy24.jpg"), user_id: user9.id)
 image26 = Image.create(image: File.open("app/assets/images/puppy25.jpg"), user_id: user8.id)
 image27 = Image.create(image: File.open("app/assets/images/puppy26.jpg"), user_id: user7.id)
+image28 = Image.create(image: File.open("app/assets/images/puppy27.jpg"), user_id: user11.id)
+image29 = Image.create(image: File.open("app/assets/images/puppy28.jpg"), user_id: user12.id)
+image30 = Image.create(image: File.open("app/assets/images/puppy29.jpg"), user_id: user13.id)
+image31 = Image.create(image: File.open("app/assets/images/puppy30.jpg"), user_id: user14.id)
+image32 = Image.create(image: File.open("app/assets/images/puppy31.jpg"), user_id: user15.id)
+image33 = Image.create(image: File.open("app/assets/images/puppy32.jpg"), user_id: user16.id)
+image34 = Image.create(image: File.open("app/assets/images/puppy33.jpg"), user_id: user17.id)
+image35 = Image.create(image: File.open("app/assets/images/puppy34.jpg"), user_id: user18.id)
+image36 = Image.create(image: File.open("app/assets/images/puppy35.jpg"), user_id: user18.id)
+image37 = Image.create(image: File.open("app/assets/images/puppy36.jpg"), user_id: user1.id)
+image38 = Image.create(image: File.open("app/assets/images/puppy37.jpg"), user_id: user20.id)
+image39 = Image.create(image: File.open("app/assets/images/puppy38.jpg"), user_id: user21.id)
+image40 = Image.create(image: File.open("app/assets/images/puppy39.jpg"), user_id: user22.id)
+image41 = Image.create(image: File.open("app/assets/images/puppy40.jpg"), user_id: user17.id)
+image42 = Image.create(image: File.open("app/assets/images/puppy41.jpg"), user_id: user15.id)
+image43 = Image.create(image: File.open("app/assets/images/puppy42.jpg"), user_id: user11.id)
+image44 = Image.create(image: File.open("app/assets/images/puppy43.jpg"), user_id: user10.id)
+image45 = Image.create(image: File.open("app/assets/images/puppy44.jpg"), user_id: user1.id)
+image46 = Image.create(image: File.open("app/assets/images/puppy45.jpg"), user_id: user18.id)
+image47 = Image.create(image: File.open("app/assets/images/puppy46.jpg"), user_id: user19.id)
+image48 = Image.create(image: File.open("app/assets/images/puppy47.jpg"), user_id: user14.id)
+image49 = Image.create(image: File.open("app/assets/images/puppy48.jpg"), user_id: user13.id)
+image50 = Image.create(image: File.open("app/assets/images/puppy49.jpg"), user_id: user12.id)
+image51 = Image.create(image: File.open("app/assets/images/puppy50.jpg"), user_id: user11.id)
+image52 = Image.create(image: File.open("app/assets/images/puppy51.jpg"), user_id: user20.id)
+image53 = Image.create(image: File.open("app/assets/images/puppy52.jpg"), user_id: user21.id)
+image54 = Image.create(image: File.open("app/assets/images/puppy53.jpg"), user_id: user22.id)
+image55 = Image.create(image: File.open("app/assets/images/puppy54.jpg"), user_id: user10.id)
 
 Like.create(liker_id: user1.id, image_id: image1.id)
 Like.create(liker_id: user1.id, image_id: image2.id)
@@ -95,7 +162,30 @@ Like.create(liker_id: user2.id, image_id: image18.id)
 Like.create(liker_id: user3.id, image_id: image19.id)
 Like.create(liker_id: user6.id, image_id: image12.id)
 Like.create(liker_id: user7.id, image_id: image12.id)
-Like.create(liker_id: user8.id, image_id: image12.id)
+Like.create(liker_id: user11.id, image_id: image12.id)
+Like.create(liker_id: user12.id, image_id: image13.id)
+Like.create(liker_id: user13.id, image_id: image27.id)
+Like.create(liker_id: user14.id, image_id: image28.id)
+Like.create(liker_id: user15.id, image_id: image29.id)
+Like.create(liker_id: user16.id, image_id: image30.id)
+Like.create(liker_id: user17.id, image_id: image31.id)
+Like.create(liker_id: user18.id, image_id: image32.id)
+Like.create(liker_id: user19.id, image_id: image32.id)
+Like.create(liker_id: user20.id, image_id: image33.id)
+Like.create(liker_id: user21.id, image_id: image35.id)
+Like.create(liker_id: user22.id, image_id: image40.id)
+Like.create(liker_id: user2.id, image_id: image46.id)
+Like.create(liker_id: user4.id, image_id: image50.id)
+Like.create(liker_id: user9.id, image_id: image36.id)
+Like.create(liker_id: user16.id, image_id: image42.id)
+Like.create(liker_id: user19.id, image_id: image55.id)
+Like.create(liker_id: user19.id, image_id: image1.id)
+Like.create(liker_id: user18.id, image_id: image1.id)
+Like.create(liker_id: user17.id, image_id: image1.id)
+Like.create(liker_id: user15.id, image_id: image1.id)
+Like.create(liker_id: user19.id, image_id: image3.id)
+Like.create(liker_id: user14.id, image_id: image7.id)
+Like.create(liker_id: user22.id, image_id: image50.id)
 
 Comment.create(body: "The puppy is soo cute!",
               author_id: user1.id,
@@ -125,7 +215,16 @@ Comment.create(body: Faker::StarWars.wookie_sentence, author_id: user3.id, image
 Comment.create(body: "The puppy is soo cute!", author_id: user2.id, image_id: image24.id)
 Comment.create(body: Faker::StarWars.wookie_sentence, author_id: user6.id, image_id: image26.id)
 Comment.create(body: "The puppy is soo cute!", author_id: user5.id, image_id: image14.id)
-Comment.create(body: Faker::StarWars.wookie_sentence, author_id: user1.id, image_id: image22.id)
+Comment.create(body: Faker::StarWars.wookie_sentence, author_id: user15.id, image_id: image2.id)
+Comment.create(body: Faker::Hacker.say_something_smart, author_id: user10.id, image_id: image3.id)
+Comment.create(body: Faker::StarWars.wookie_sentence, author_id: user19.id, image_id: image4.id)
+Comment.create(body: Faker::Hacker.say_something_smart, author_id: user20.id, image_id: image5.id)
+Comment.create(body: Faker::StarWars.wookie_sentence, author_id: user21.id, image_id: image6.id)
+Comment.create(body: Faker::Hacker.say_something_smart, author_id: user22.id, image_id: image2.id)
+Comment.create(body: Faker::StarWars.wookie_sentence, author_id: user13.id, image_id: image22.id)
+Comment.create(body: Faker::Hacker.say_something_smart, author_id: user14.id, image_id: image8.id)
+Comment.create(body: Faker::Hacker.say_something_smart, author_id: user16.id, image_id: image15.id)
+Comment.create(body: Faker::StarWars.wookie_sentence, author_id: user9.id, image_id: image22.id)
 
 Follow.create(follower_id: user1.id, followee_id: user2.id)
 Follow.create(follower_id: user1.id, followee_id: user3.id)
@@ -165,3 +264,18 @@ Follow.create(follower_id: user9.id, followee_id: user2.id)
 Follow.create(follower_id: user9.id, followee_id: user1.id)
 Follow.create(follower_id: user9.id, followee_id: user7.id)
 Follow.create(follower_id: user10.id, followee_id: user1.id)
+Follow.create(follower_id: user11.id, followee_id: user12.id)
+Follow.create(follower_id: user13.id, followee_id: user14.id)
+Follow.create(follower_id: user15.id, followee_id: user16.id)
+Follow.create(follower_id: user16.id, followee_id: user17.id)
+Follow.create(follower_id: user17.id, followee_id: user18.id)
+Follow.create(follower_id: user19.id, followee_id: user20.id)
+Follow.create(follower_id: user21.id, followee_id: user22.id)
+Follow.create(follower_id: user20.id, followee_id: user1.id)
+Follow.create(follower_id: user19.id, followee_id: user1.id)
+Follow.create(follower_id: user18.id, followee_id: user1.id)
+Follow.create(follower_id: user15.id, followee_id: user1.id)
+Follow.create(follower_id: user12.id, followee_id: user15.id)
+Follow.create(follower_id: user13.id, followee_id: user19.id)
+Follow.create(follower_id: user14.id, followee_id: user22.id)
+Follow.create(follower_id: user19.id, followee_id: user12.id)
