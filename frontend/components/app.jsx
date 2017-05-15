a2c2e8fadb2e680f8c810d0066bc390eea5819b5
Maggie Liu <mliu0805@gmail.com>
2017-05-15 10:08:35 -0400

    Add footer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBarContainer from './navbar/navbar_container';
+import Footer from './footer/footer';
 
 const App = ({ children }) => (
   <div>
     <NavBarContainer />
     { children }
+    <Footer />
   </div>
 );
 
---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+    <footer>
+      <ul className='footer'>
+        <li><a href="https://github.com/liumaggie/instagram">Github</a></li>
+        <li><a href="http://maggieliu.me/">Portfolio</a></li>
+        <li><a href="https://www.linkedin.com/in/maggie-liu">LinkedIn</a></li>
+      </ul>
+      <p>© Maggie Liu 2017</p>
+    </footer>
+  );
+};
+
+export default Footer;
