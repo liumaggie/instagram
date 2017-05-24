5f87fe27788e024cfab305d09749034bd5f44e93
Maggie Liu <mliu0805@gmail.com>
2017-05-24 12:01:14 -0400

    Check user profile

--- frontend/components/user_profile/user_profile.jsx
+++ frontend/components/user_profile/user_profile.jsx
@@ -29,7 +29,8 @@ class UserProfile extends React.Component {
     this.props.fetchUser(this.parseParamsId(props))
       .then(() => this.props.fetchImagesForUser(
                                   this.parseParamsId(props)))
-      .then(() => this.setState({ loading: false }));
+      .then(() => this.setState({ loading: false }))
+      .then(() => console.log(this.props.user))
   }
 
   componentWillReceiveProps(nextProps) {
